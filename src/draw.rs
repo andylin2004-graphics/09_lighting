8d7653cba93b2c61c3d05198d8f847f24330931d
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-05-05 17:40:42 -0400

    fixed scanline code

--- src/draw.rs
+++ src/draw.rs
@@ -213,23 +213,17 @@ impl Image {
         let mut x1 = polygons[0].0;
         let mut z0 = polygons[0].2;
         let mut z1 = polygons[0].2;
-        let y0 = polygons[0].1;
-        let dx0 = if (polygons[2].1 - polygons[0].1) as i32 > 0 {(polygons[2].0 - polygons[0].0) / (polygons[2].1 - polygons[0].1)} else {0.0};
-        let dz0 = if (polygons[2].1 - polygons[0].1) as i32 > 0 {(polygons[2].2 - polygons[0].2) / (polygons[2].1 - polygons[0].1)} else {0.0};
-        let mut dx1 = if (polygons[1].1 - polygons[0].1) as i32 > 0 {(polygons[1].0 - polygons[0].0) / (polygons[1].1 - polygons[0].1)} else {0.0};
-        let mut dz1 = if (polygons[1].1 - polygons[0].1) as i32 > 0 {(polygons[1].2 - polygons[0].2) / (polygons[1].1 - polygons[0].1)} else {0.0};
-        let dx1_1 = if (polygons[2].1 - polygons[1].1) as i32 > 0 {(polygons[2].0 - polygons[1].0) / (polygons[2].1 - polygons[1].1)} else {0.0};
-        let dz1_1 = if (polygons[2].1 - polygons[1].1) as i32 > 0 {(polygons[2].2 - polygons[1].2) / (polygons[2].1 - polygons[1].1)} else {0.0};
+        let dx0 = if (polygons[2].1 as i32 - polygons[0].1 as i32) + 1 > 0 {(polygons[2].0 - polygons[0].0) / ((polygons[2].1 as i32 - polygons[0].1 as i32) + 1) as f32} else {0.0};
+        let dz0 = if (polygons[2].1 as i32 - polygons[0].1 as i32) + 1 > 0 {(polygons[2].2 - polygons[0].2) / ((polygons[2].1 as i32 - polygons[0].1 as i32) + 1) as f32} else {0.0};
+        let mut dx1 = if (polygons[1].1 as i32 - polygons[0].1 as i32) + 1 > 0 {(polygons[1].0 - polygons[0].0) / ((polygons[1].1 as i32 - polygons[0].1 as i32) + 1) as f32} else {0.0};
+        let mut dz1 = if (polygons[1].1 as i32 - polygons[0].1 as i32) + 1 > 0 {(polygons[1].2 - polygons[0].2) / ((polygons[1].1 as i32 - polygons[0].1 as i32) + 1) as f32} else {0.0};
+        let dx1_1 = if (polygons[2].1 as i32 - polygons[1].1 as i32) + 1 > 0 {(polygons[2].0 - polygons[1].0) / ((polygons[2].1 as i32 - polygons[1].1 as i32) + 1) as f32} else {0.0};
+        let dz1_1 = if (polygons[2].1 as i32 - polygons[1].1 as i32) + 1 > 0 {(polygons[2].2 - polygons[1].2) / ((polygons[2].1 as i32 - polygons[1].1 as i32) + 1) as f32} else {0.0};
         if (polygons[2].1 - polygons[1].1) as i32 == 0{
             past_midpoint = true
         }
         // for y in y0 as i32..=y0 as i32+1 {
-        for y in y0 as i32..=polygons[2].1 as i32{
-            self.draw_line((x0 as i32).abs(), y, z0, (x1 as i32).abs(), y, z1, color);
-            x0 += dx0;
-            x1 += dx1;
-            z0 += dz0;
-            z1 += dz1;
+        for y in polygons[0].1 as i32..=polygons[2].1 as i32{
             if y >= polygons[1].1 as i32 && !past_midpoint{
                 dx1 = dx1_1;
                 dz1 = dz1_1;
@@ -237,6 +231,11 @@ impl Image {
                 z1 = polygons[1].2;
                 past_midpoint = true;
             }
+            self.draw_line((x0 as i32).abs(), y, z0, (x1 as i32).abs(), y, z1, color);
+            x0 += dx0;
+            x1 += dx1;
+            z0 += dz0;
+            z1 += dz1;
         }
     }
 }
